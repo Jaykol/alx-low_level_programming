63efe02fb57483fcf57b19bd0fcb9a4d2ea002ef
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-09 03:37:27 -0700

    entry function to print text using 'puts'

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
      /**
-      *program to print a text
-      * returns (0)
+      * program to print a text
+      * returns 0
       */
 int main(void)
-	/**
-	 * main -entry point
-	 */
 	{
 		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
