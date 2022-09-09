ae208b71cbc352f689946e263c09f1d53f0cae2b
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-09 11:21:32 -0700

    print sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - code block for sizeof
+ *return 0
+ */
+int main(void)
+{
+	printf("size of char is: " sizeof(char));
+	printf("Size of int is: " sizeof(int));
+	printf("Size of long int is: " sizeof(long));
+	printf("Size of float is: " sizeof(float));
+	
+	return (0)
+}
