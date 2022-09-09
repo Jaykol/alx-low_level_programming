d87b998bac8412c2099ff032424c571569b8a18c
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-09 10:19:51 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
+ * main - entry code block
+ * return 0
  *code block showing printf usage
  */
 
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
