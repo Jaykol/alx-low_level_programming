ba0a4df361dfcb810b328349e923c1c79f6fb32f
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-12 22:09:10 -0700

    solution

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - main block
- * return: 0
+ * Return: 0
  */
 
 int main(void)
@@ -10,7 +10,7 @@ int main(void)
 
 	for (x = 'a'; x <= 'z'; x++)
 	{
-		putchar(x);
+		putchar("%s\n", x);
 	}
 	return (0);
 }
