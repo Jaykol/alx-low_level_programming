7172748e225fe102a08592c0653ffd5a91ac1236
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-13 03:34:33 -0700

    prints combination of given number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int i,j;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar(i, j);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
