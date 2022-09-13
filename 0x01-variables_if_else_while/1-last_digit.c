767506b3a9176e346e4fa227a13e9831e2f9990d
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-12 21:23:01 -0700

    prints last digits of random numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
+
 /**
  * main - main code block
  * Returns: 0
  */
-/* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
