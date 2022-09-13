f2229205b76af5e870d90a34ec7697b1864d715c
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-12 22:39:23 -0700

    prints alphabets in both upper and lower case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,9 @@ int main(void)
 {
 	char x;
 
-	for(x = 'A'; x <= 'Z'; x++)
-	
-	putchar(x);
-	
-	putchar(tolower(x));
-	
+	for (x = 'A'; x <= 'Z'; x++)
+	putchar(x && tolower(x));
+
 	putchar('\n');
-	
 	return (0);
 }
