38e4a871b097f62e1a8d7e60edf02ea87c05e3d2
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-14 15:29:20 -0700

    corrections

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,13 +13,11 @@ void print_alphabet_x10(void)
 	while (c < 10)
 	{
 		c = 'a';
-		while (c <= 'z')
+		for(c; c <= 'z', c++)
 		{
 			_putchar(c);
-			c++;
 		}
 		_putchar('\n');
 		i++;
 	}
-	return (c);
 }
