562f0356bd224e79994fda51fb1183c1615ec7f4
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-14 17:56:32 -0700

    correction

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,7 +6,12 @@ int print_last_digit(int n)
 {
 	int lastdigit = n % 10;
 
-	_putchar(lastdigit);
-
-	return (0);
+	if (n < 0)
+	{
+		_putchar(-1 * lastdigit + '0');
+	}
+	else
+		_putchar(lastdigit + '0');
+	
+	return (lastdigit);
 }
