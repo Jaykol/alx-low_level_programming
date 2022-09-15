1d10d2d7773187cd6fd5a6829c07bfc07b37afbc
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-14 17:00:18 -0700

    checks for lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,15 +3,16 @@
 
 /**
  *  _islower - checks if a character is lower case
- * Return: 1
- * Return: 0
+ * Return: 1 or 0
+ * @c: type int
  */
 
 int _islower(int c)
 {
 	if (c <= 123 || c >= 97)
 	{
-		return(1);
+		return (1);
 	}
-	return (0);
+	else
+		return (0);
 }
