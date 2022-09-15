ea77f0243deaa889f5213130f386071283b67caf
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-14 19:27:43 -0700

    solution

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char _putchar[] = "main";
+	char main[] = "main";
 	int i = 0;
 
-	while (_putchar[i] != '\0')
+	while (main[i] != '\0')
 	{
-		_putchar(_putchar[i]);
+		_putchar(main[i]);
 		i++;
 	}
 	_putchar('\n');
