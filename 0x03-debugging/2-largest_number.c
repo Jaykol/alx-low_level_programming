932abb7af7dc2caaca2b8c75df4913b92139f3df
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-20 02:23:37 -0700

    new changes

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,18 +15,15 @@ int largest_number(int a, int b, int c)
     if (a > b && b > c)
     {
         largest = a;
-	return (largest);
     }
     else if (b > a && a > c)
     {
         largest = b;
-	return (largest);
     }
     else
     {
         largest = c;
-	return (largest);
     }
 
-    return (0);
+    return (largest);
 }
