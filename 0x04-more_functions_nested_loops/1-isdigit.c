455872c1c0c7962bda801a606fcf3812fbf80916
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-22 01:30:31 -0700

    checks if c is a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -14,17 +14,17 @@
 int _isdigit(int c)
 
 {
-	char i = '0';
-	int isdigit = 0;
+	int i = 0;
 
-	for (; i <= '9'; i++)
+	for (; i <= 9; i++)
 	{
 		if (i == c)
 		{
-			isdigit = 1;
-			break;
+			return (1);
 		}
+		else
+			return (0);
 	}
-	return (isdigit);
+	return (0);
 
 }
