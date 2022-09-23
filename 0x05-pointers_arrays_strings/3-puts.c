8d7f0e0e8c803a8019640c0b05dea92f5128769d
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-22 17:51:16 -0700

    print to stdout using puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ *_puts - print a string to stdout
+ * @str: char array string type
+ * Description: Can only use _putchar
+ */
+void _puts(char *str)
+{
+	puts(str);
+
+	return (0);
+}
