85377fd5bd632e6cf81536119bb16619fefeb9f1
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-22 17:36:28 -0700

    prints length of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 /*
  *_strlen - return the length of the string
- * @*s: char type pointer
+ * @s: char type pointer
  * Return: length of string
  */
 int _strlen(char *s)
 {
-	_putchar(strlen(*s));
+	_putchar(strlen(s));
 	return (0);
 }
