e3ea1d00e83fd17a86a7959bf4c122a5e276c290
Jesutofunmi <jekola.ajekola@gmail.com>
2022-09-27 07:17:09 -0700

    solution

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * unsigned int _strspn - a function that gets the length of a prefix substring
+ * @s:
+ * @accept:
+ * Return: 
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (size_t strspn(const char *s, const char *accept));
+}
