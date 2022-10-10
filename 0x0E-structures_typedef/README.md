b451fc90122c519457c7291444d233ac6187318d
Jesutofunmi <jekola.ajekola@gmail.com>
2022-10-10 04:09:54 -0700

    first commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+tasks on Struct and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+int main(void)
+{
+	struct dog my_dog;
+	return (0);
+}
