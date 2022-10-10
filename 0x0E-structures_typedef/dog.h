0d2705d4c154e0ee2278db57350b5dedd94b2d3f
Jesutofunmi <jekola.ajekola@gmail.com>
2022-10-10 04:15:15 -0700

    first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,17 +3,20 @@
 #include <stdlib.h>
 
 /**
- *
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-int main(void)
-{
-	struct dog my_dog;
-	return (0);
-}
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
