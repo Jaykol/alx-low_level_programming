8f822876ad49249a371d1c5f65107a7b1fdb46ff
Jesutofunmi <jekola.ajekola@gmail.com>
2022-10-11 03:22:55 -0700

    solution

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,4 +1,4 @@
-#ifndef SUM_FUNC_H
-#define SUM_FUNC_H
-#define SUM(x,y) ((x) + (y))
-#endif /* SUM_FUNC_H */
+#ifndef FOUR_SUM_H
+#define FOUR_SUM_H
+#define SUM(x, y) ((x) + (y))
+#endif /* FOUR_SUM_H */
