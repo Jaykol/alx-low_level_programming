25bbeef0e26a0ade59872dfaf28dc0cc5d985db4
Jesutofunmi <jekola.ajekola@gmail.com>
2022-10-11 02:51:05 -0700

    solution

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the name of where the file is compiled from
+ * Return: 0;
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
