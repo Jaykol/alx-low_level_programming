789fb3817436768354f30fadb9f65d653ad1aa77
Jesutofunmi <jekola.ajekola@gmail.com>
2022-11-02 06:19:50 -0700

    solutions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,18 +18,16 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, fdw, len = 0;
-	char *buffer;
 
-	if (filename == -1)
+	if (filename == NULL)
 		return (-1);
-
-	buffer = malloc(sizeof(char) * len);
-	if (buffer == NULL)
-		return (0);
 
 	fd = open(filename, 0_RDWR | 0_CREAT | 0_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
+
+	while (text_content && *(text_content + len))
+		len++;
 
 	fdw = write(fd, text_content, len);
 	close(fd);
